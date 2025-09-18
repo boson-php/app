bbab8522c50f459cd163d2fca879dd00af18f4b1
Kirill Nesmeyanov <nesk@xakep.ru>
2025-09-18 04:39:44 +0300

    Improve configuration

--- index.php
+++ index.php
@@ -14,39 +14,141 @@ use Boson\Window\WindowDecoration;
 
 require __DIR__ . '/vendor/autoload.php';
 
+
+/**
+ * -----------------------------------------------------------------------------
+ *   Boson Application
+ * -----------------------------------------------------------------------------
+ *
+ * Creates a new Boson Application.
+ *
+ * Don't be afraid to modify the configuration!
+ *
+ */
+
 $app = new Application(new ApplicationCreateInfo(
-    schemes: ['boson'],
+    /**
+     * @link https://bosonphp.com/doc/master/schemes-api#registration
+     */
+    schemes: [ 'boson' ],
+    /**
+     * @link https://bosonphp.com/doc/master/application-configuration#debug-mode
+     */
     debug: false,
+    /**
+     * @link https://bosonphp.com/doc/master/window-configuration
+     */
     window: new WindowCreateInfo(
+        /**
+         * @link https://bosonphp.com/doc/master/window-configuration#window-size-width-and-height
+         */
         width: 800,
+        /**
+         * @link https://bosonphp.com/doc/master/window-configuration#window-size-width-and-height
+         */
         height: 600,
+        /**
+         * @link https://bosonphp.com/doc/master/window-configuration#window-decorations
+         */
         decoration: WindowDecoration::DarkMode,
         webview: new WebViewCreateInfo(
             extensions: [
+                /**
+                 * Load default webview extensions.
+                 */
                 ...WebViewCreateInfo::DEFAULT_WEBVIEW_EXTENSIONS,
+                /**
+                 * Extend by the `WebComponentsExtensionProvider` from
+                 * "boson-php/webview-ext-web-components" dependency.
+                 */
                 new WebComponentsExtensionProvider(),
             ]
         ),
     ),
 ));
 
-$static = new FilesystemStaticProvider([
-    __DIR__ . '/assets',
-]);
 
-$app->on(static function (SchemeRequestReceived $e) use ($static): void {
-    $e->response = $static->findFileByRequest($e->request);
 
-    if ($e->response !== null) {
-        return;
-    }
-
-    $e->response = new Response(
-        body: file_get_contents(__DIR__ . '/assets/view/layout/main.html'),
-    );
-});
+/**
+ * -----------------------------------------------------------------------------
+ *   Add WebView Components
+ * -----------------------------------------------------------------------------
+ *
+ * Adds support for the specified custom tags.
+ *
+ * @link https://bosonphp.com/doc/master/web-components-api
+ *
+ */
 
 $app->webview->components->add('app-logo', \App\Components\Logo::class);
 $app->webview->components->add('app-headlines', \App\Components\Headlines::class);
+
+
+/**
+ * -----------------------------------------------------------------------------
+ *   FS Static Provider
+ * -----------------------------------------------------------------------------
+ *
+ * Provides functionality for accessing files on the local
+ * file system based on HTTP requests.
+ *
+ * You may also create an `override` directory next to the compiled binary to
+ * override all assets.
+ *
+ * For example:
+ * - build/windows/amd64/app.exe
+ * - build/windows/amd64/assets/xxx
+ *
+ * Directory and file mounting rules settings are located in the `mount`
+ * section of the `boson.json` config.
+ *
+ */
+
+$static = new FilesystemStaticProvider([
+    __DIR__ . '/assets.override',
+    __DIR__ . '/assets',
+]);
+
+
+
+/**
+ * -----------------------------------------------------------------------------
+ *   Request Handler
+ * -----------------------------------------------------------------------------
+ *
+ * Process all "request" events from Boson Application.
+ *
+ * @link https://bosonphp.com/doc/master/schemes-api#requests-interception
+ *
+ */
+
+$app->on(static function (SchemeRequestReceived $e) use ($static): void {
+    //
+    // Install file (if exists) as response using static adapter...
+    //
+    if ($e->response = $static->findFileByRequest($e->request)) {
+        $e->stopPropagation();
+
+        return;
+    }
+
+    $response = file_get_contents(__DIR__ . '/assets/view/layout/main.html');
+
+    $e->response = new Response($response);
+});
+
+
+
+/**
+ * -----------------------------------------------------------------------------
+ *   Fire Request
+ * -----------------------------------------------------------------------------
+ *
+ * Go to the specified address, which in turn initializes the
+ * first event of the incoming request.
+ *
+ * @link https://bosonphp.com/doc/master/webview#url-navigation
+ *
+ */
 
 $app->webview->url = 'boson://index';
